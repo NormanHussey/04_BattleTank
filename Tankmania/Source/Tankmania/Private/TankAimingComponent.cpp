8450a7836cbd02c4f1e344c30af87fe6a79fc018
FEYNMACHINE\Norman Hussey <norm.hussey@gmail.com>
2018-01-17 21:08:33 -0500

    Lecture 141

--- Tankmania/Source/Tankmania/Private/TankAimingComponent.cpp
+++ Tankmania/Source/Tankmania/Private/TankAimingComponent.cpp
@@ -11,7 +11,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true; // Should this really tick?
 
 	// ...
 }
@@ -40,7 +40,13 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 	auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 	MoveBarrelTowards(AimDirection);
-	// UE_LOG(LogTemp, Warning, TEXT("%s aiming at: %s"), *TankName, *AimDirection.ToString());
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f Aim Solution Found"), Time);
+	}
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f No Aim Solve Found"), Time);
 	}
 	// If no solution found do nothing
 }
