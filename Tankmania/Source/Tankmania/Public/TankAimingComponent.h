d14e55e804b6c993de8a57549dd273dac4f6d82e
FEYNMACHINE\Norman Hussey <norm.hussey@gmail.com>
2018-01-17 19:58:16 -0500

    Lecture 139

--- Tankmania/Source/Tankmania/Public/TankAimingComponent.h
+++ Tankmania/Source/Tankmania/Public/TankAimingComponent.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+class UTankBarrel; // Forward declaration
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TANKMANIA_API UTankAimingComponent : public UActorComponent
@@ -16,7 +17,7 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+	void SetBarrelReference(UTankBarrel* BarrelToSet);
 	
 	// TODO add SetTurretReference
 
@@ -29,7 +30,7 @@ public:
 
 private:
 
-	UStaticMeshComponent * Barrel = nullptr;
+	UTankBarrel * Barrel = nullptr;
 
 	void MoveBarrelTowards(FVector AimDirection);
 	
