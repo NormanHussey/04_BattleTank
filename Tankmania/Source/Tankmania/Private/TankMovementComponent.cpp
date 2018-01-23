fc836c3ee82d9e9638c7154395ebef3b2b5069de
FEYNMACHINE\Norman Hussey <norm.hussey@gmail.com>
2018-01-22 21:05:56 -0500

    Lecture 162

--- Tankmania/Source/Tankmania/Private/TankMovementComponent.cpp
+++ Tankmania/Source/Tankmania/Private/TankMovementComponent.cpp
@@ -5,7 +5,7 @@
 
 void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 
@@ -14,12 +14,19 @@ void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	if (!LeftTrack || !RightTrack) { return; }
 
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 	// TODO Prevent double throttle due to dual control use
 }
 
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+//	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	if (!LeftTrack || !RightTrack) { return; }
 
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+	
+}
