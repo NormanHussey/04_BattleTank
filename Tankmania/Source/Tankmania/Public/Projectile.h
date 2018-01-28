cacc213061c7ae76c3af577a8303d554d8ea6788
FEYNMACHINE\Norman Hussey <norm.hussey@gmail.com>
2018-01-27 21:31:49 -0500

    Lecture 193

--- Tankmania/Source/Tankmania/Public/Projectile.h
+++ Tankmania/Source/Tankmania/Public/Projectile.h
@@ -21,8 +21,6 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 	void LaunchProjectile(float Speed);
 
@@ -35,4 +33,9 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* LaunchBlast = nullptr;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UParticleSystemComponent* ImpactBlast = nullptr;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 };
